7673cf335e2c077da76b1df260a27f1b826834e7
Wasif Khan <waasif.khaan@gmail.com>
2018-09-22 20:02:59 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,9 @@ describe Post do
     expect(post).to be_valid
   end
 
+  it 'has a summary' do
+  post = Post.create!(title: "My title", description: "The post description")
+  expect(post.post_summary).to eq("My title - The post description")
+end
+
 end
